31c7e469ec465ebb987f4e1372c2fe087138418b
ssmith <shawn.t.smith.777@gmail.com>
2020-07-26 21:17:19 -0500

    init commit

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var nums = [1, 2, 3, 4];
+var doubleNums = nums.map(function (e) {
+  return e * 2;
+});
+console.log(doubleNums);
---
+++ src/app.js
@@ -0,0 +1,3 @@
+var nums = [1, 2, 3, 4, 5];
+var doubleNums = nums.map((e) => e * 2);
+console.log(doubleNums);
